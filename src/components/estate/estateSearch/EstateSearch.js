040c61128fee82be5fea9c994dc6549cdb8143dd
lsenaa <lsenaa3838@gmail.com>
2024-11-09 23:26:38 +0900

    design: 매물검색 메뉴선택시 효과 추가

--- src/components/estate/estateSearch/EstateSearch.js
+++ src/components/estate/estateSearch/EstateSearch.js
@@ -1,43 +1,77 @@
-import styles from './EstateSearch.module.scss';
-import { useState } from 'react';
-import EstateList from '../estateList/EstateList';
-import EstateDetail from '../estateDetail/EstateDetail';
-import { Modal } from 'antd';
+import styles from "./EstateSearch.module.scss";
+import { useState } from "react";
+import EstateList from "../estateList/EstateList";
+import EstateDetail from "../estateDetail/EstateDetail";
+import { Modal } from "antd";
 
 const EstateSearch = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [type, setType] = useState(0);
 
   const handleCancel = () => {
     setIsModalOpen(!isModalOpen);
-  }
+  };
+
+  const onClickType = (index) => () => {
+    setType(index);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.topWrapper}>
         <ul className={styles.typeSelect}>
-          <li>시골농가주택</li>
-          <li>전원주택</li>
-          <li>아파트/빌라</li>
-          <li>농장/토지</li>
+          <li
+            className={type === 0 ? styles.selected : undefined}
+            style={{ "--selected-index": type }}
+            onClick={onClickType(0)}
+          >
+            시골농가주택
+          </li>
+          <li
+            className={type === 1 ? styles.selected : undefined}
+            style={{ "--selected-index": type }}
+            onClick={onClickType(1)}
+          >
+            전원주택
+          </li>
+          <li
+            className={type === 2 ? styles.selected : undefined}
+            style={{ "--selected-index": type }}
+            onClick={onClickType(2)}
+          >
+            아파트/빌라
+          </li>
+          <li
+            className={type === 3 ? styles.selected : undefined}
+            style={{ "--selected-index": type }}
+            onClick={onClickType(3)}
+          >
+            농장/토지
+          </li>
         </ul>
         <div className={styles.searchWrapper}>
-          <input type="test" placeholder="매물을 검색해주세요."/>
+          <input type="test" placeholder="매물을 검색해주세요." />
           <button>검색</button>
         </div>
       </div>
 
       <div className={styles.bodyWrapper}>
-        <EstateList isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+        <EstateList isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
         {/* 지도 들어감 */}
       </div>
-      {isModalOpen &&
-        <Modal open={isModalOpen} onCancel={handleCancel} width={857} footer={null} className={styles.customModal}>
+      {isModalOpen && (
+        <Modal
+          open={isModalOpen}
+          onCancel={handleCancel}
+          width={857}
+          footer={null}
+          className={styles.customModal}
+        >
           <EstateDetail />
         </Modal>
-      }
+      )}
     </div>
-
   );
-}
+};
 
 export default EstateSearch;
