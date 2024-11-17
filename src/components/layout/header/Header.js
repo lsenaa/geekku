75b16c7e400dac95f23723e6815399fdad80a733
lsenaa <lsenaa3838@gmail.com>
2024-11-17 17:17:58 +0900

    design: 로그인 헤더 구현

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -4,10 +4,27 @@ import styles from './Header.module.scss';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NotificationButton from 'components/layout/notification/NotificationButton';
+import { IoMdNotificationsOutline } from 'react-icons/io';
+import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(true);
   const [write, setWrite] = useState(false);
   const navigate = useNavigate();
+  console.log(write);
+
+  const userWrite = [
+    { name: '집꾸 신청하기', path: '/house/write' },
+    { name: '방꾸 신청하기', path: '/requestInteriorWrite' },
+    { name: '한번에 꾸하기 신청하기', path: '/oneStopWrite' },
+    { name: '집들이 글쓰기', path: '/communityBoardWrite' },
+    { name: '인테리어 후기 작성하기', path: '/' },
+  ];
+  const estateWrite = { name: '매물 등록하기', path: '/estate/write' };
+  const interiorWrite = [
+    { name: '시공업체 등록하기', path: '/companyRegister' },
+    { name: '시공사례 등록하기', path: '/' },
+  ];
 
   return (
     <header className={styles.Container}>
@@ -33,24 +50,40 @@ const Header = () => {
           </li>
         </ul>
       </nav>
+      {/* 로그인 유무 헤더 */}
+      {isLogin ? (
+        <div className={styles.loginMenuWrap}>
+          <button className={styles.btn} onClick={() => setWrite(!write)}>
+            글쓰기
+          </button>
+          {write && (
+            <ul className={styles.writeWrapper}>
+              {userWrite.map((user, i) => (
+                <li key={i}>
+                  <Link to={user.path} onClick={() => setWrite(!write)}>
+                    {user.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          )}
+          <div className={styles.notificationWrap}>
+            <IoMdNotificationsOutline size="30" color="#6D885D" />
+            <p>10</p>
+          </div>
+          <div className={styles.userProfile}>
+            <FaUserCircle size="30" color="#6D885D" />
+            <p className={styles.name}>홍길동</p>
+          </div>
+        </div>
+      ) : (
+        <button onClick={() => navigate('/Login')} className={styles.btn}>
+          로그인 | 회원가입
+        </button>
+      )}
+
       {/* 알림 버튼 추가 위치 옮겨야 함*/}
-      <NotificationButton />
-
-      <button onClick={() => navigate('/Login')} className={styles.btn}>
-        로그인 | 회원가입
-      </button>
-
-      {/* 글쓰기 버튼 */}
-      {/* <button className={styles.btn} onClick={() => setWrite(!write)}>글쓰기</button>
-      {write && 
-        <ul className={styles.writeWrapper}>
-          <li><Link to={"/"}>집꾸 신청하기</Link></li>
-          <li><Link to={"/"}>방꾸 신청하기</Link></li>
-          <li><Link to={"/"}>한번에 꾸하기 신청하기</Link></li>
-          <li><Link to={"/"}>집들이 글쓰기</Link></li>
-          <li><Link to={"/"}>인테리어 후기 작성하기</Link></li>
-        </ul>
-      } */}
+      {/* <NotificationButton /> */}
     </header>
   );
 };
