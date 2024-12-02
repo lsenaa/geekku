cc464fe8373d6ba820e9d313098c0e1a93b8773b
rena <dayoungrenakang@gmail.com>
2024-12-02 21:34:47 +0900

    feat : mypage 내에서 비밀번호 변경 기능 추가

--- src/components/mypage/info/ModifyPwd.js
+++ src/components/mypage/info/ModifyPwd.js
@@ -1,31 +1,98 @@
 import styles from '../../login/Login.module.scss';
+import axios from 'axios';
+import { url } from 'lib/axios';
+import { useState } from 'react';
+import { Modal } from 'antd';
+import { useAtom } from 'jotai';
+import { userAtom, tokenAtom } from 'store/atoms';
+
 const ModifyPwd = () => {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [user, setUser] = useAtom(userAtom);
+  const [token, setToken] = useAtom(tokenAtom);
+
+  const handleChangePassword = () => {
+    if (newPassword !== confirmPassword) {
+      Modal.error({
+        content: '비밀번호와 비밀번호 확인이 일치하지 않습니다.',
+      });
+      return;
+    }
+
+    const typeEndPoint =
+      user.role === 'ROLE_COMPANY'
+        ? `${url}/company/changePwd`
+        : `${url}/user/changePwd`;
+
+    axios
+      .post(
+        typeEndPoint,
+        { currentPassword, newPassword },
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      )
+      .then((res) => {
+        Modal.success({
+          content: '비밀번호가 변경되었습니다.',
+        });
+        if (res.data.token) {
+          setToken(res.data.token);
+        }
+      })
+      .catch((err) => {
+        console.error('비밀번호 변경 실패 :', err);
+        Modal.error({
+          content: '비밀번호 변경에 실패했습니다.',
+        });
+      });
+  };
   return (
-    <>
-      <div className={styles.searchPwd}>
-        <div className={styles.inputGroup}>
-          <span>
-            새 비밀번호<b>*</b>
-          </span>
-          <input
-            type="password"
-            placeholder="새로운 비밀번호를 입력해주세요."
-            className={styles.input}
-          />
-        </div>
-        <div className={styles.inputGroup}>
-          <span>
-            비밀번호 확인<b>*</b>
-          </span>
-          <input
-            type="password"
-            placeholder="새로운 비밀번호를 한번 더 입력해주세요."
-            className={styles.input}
-          />
-        </div>
-        <button className={styles.button}>비밀번호 변경</button>
+    <div className={styles.searchPwd}>
+      <div className={styles.inputGroup}>
+        <span>
+          현재 비밀번호<b>*</b>
+        </span>
+        <input
+          type="password"
+          placeholder="현재 비밀번호를 입력해주세요."
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+          className={styles.input}
+        />
       </div>
-    </>
+      <div className={styles.inputGroup}>
+        <span>
+          새 비밀번호<b>*</b>
+        </span>
+        <input
+          type="password"
+          placeholder="새로운 비밀번호를 입력해주세요."
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          className={styles.input}
+        />
+      </div>
+      <div className={styles.inputGroup}>
+        <span>
+          비밀번호 확인<b>*</b>
+        </span>
+        <input
+          type="password"
+          placeholder="새로운 비밀번호를 한번 더 입력해주세요."
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          className={styles.input}
+        />
+      </div>
+      <button className={styles.button} onClick={handleChangePassword}>
+        비밀번호 변경
+      </button>
+    </div>
   );
 };
 
