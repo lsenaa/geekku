965cfec8aadd1d6c7959ed9e62e1d0add646cbed
lsenaa <lsenaa3838@gmail.com>
2024-12-03 10:36:06 +0900

    design: 시공사례 상세 디자인 수정

--- src/components/interior/sampleDetail/SampleDetail.js
+++ src/components/interior/sampleDetail/SampleDetail.js
@@ -5,40 +5,52 @@ import houseType from '../../../assets/images/houseType.png';
 import size from '../../../assets/images/size.png';
 import possible from '../../../assets/images/possible.png';
 import area from '../../../assets/images/area.png';
+import Button01 from 'components/commons/button/Button01';
+import { useNavigate } from 'react-router';
 
 const SampleDetail = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <img src={cover} alt="커버사진" width="100%" height="595px" />
+      <div className={styles.coverImgWrap}>
+        <img src={cover} alt="커버사진" />
+      </div>
       <div className={styles.all}>
         <div className={styles.title}>
           허름했던 농가주택을 살기 좋은 집으로 대변신 !
         </div>
-        <div>
+        <div className={styles.contentWrap}>
           <div className={styles.user}>
-            <img src={icon} alt="유저아이콘" style={{ height: '40px' }} />
+            <img src={icon} alt="유저아이콘" style={{ height: '30px' }} />
             <div className={styles.nameInfo}>
-              <span id={styles.name}>코스타 인테리어</span>
-              <span id={styles.info}>모던 is done..</span>
+              <p id={styles.name}>코스타 인테리어</p>
+              <p id={styles.info}>모던 is done..</p>
             </div>
           </div>
           <div className={styles.type}>
-            <span>
-              <img src={houseType} alt="주거형태" /> 농가주택
-            </span>
-            <span>
-              <img src={size} alt="평수" /> 60평
-            </span>
-            <span>
-              <img src={possible} alt="부분시공 가능여부" /> 전체시공
-            </span>
-            <span>
-              <img src={area} alt="지역" /> 강원도
-            </span>
+            <div>
+              <img src={houseType} alt="주거형태" />
+              <p>농가주택</p>
+            </div>
+            <div>
+              <img src={size} alt="평수" />
+              <p>60평</p>
+            </div>
+            <div>
+              <img src={possible} alt="부분시공 가능여부" />
+              <p>전체시공</p>
+            </div>
+            <div>
+              <img src={area} alt="지역" />
+              <p>강원도</p>
+            </div>
           </div>
           <div className={styles.content}>에디터 내용 ~~~~~~</div>
         </div>
-        <div></div>
+        <Button01 size="small" onClick={() => navigate('/sampleList')}>
+          목록으로
+        </Button01>
       </div>
     </div>
   );
