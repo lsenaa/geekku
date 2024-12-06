ecd89f9d6e139c70383c28b09aaf328f307a3d46
lsenaa <lsenaa3838@gmail.com>
2024-12-06 14:47:36 +0900

    design: 시공사례 리스트 디자인 수정

--- src/components/interior/sampleList/Card.js
+++ src/components/interior/sampleList/Card.js
@@ -1,3 +1,4 @@
+import { url } from 'lib/axios';
 import img from '../../../assets/images/interiorEx.png';
 import styles from './Card.module.scss';
 import { Link } from 'react-router-dom';
@@ -12,10 +13,9 @@ const Card = ({ sampleList }) => {
           className={styles.card}
         >
           <div className={styles.fix}>
-            <img
-              src={sample.coverImage}
-              style={{ width: '384px', height: '242px' }}
-            />
+            <div className={styles.imgWrap}>
+              <img src={`${url}/sampleImage/${sample.coverImage}`} />
+            </div>
             <div className={styles.wrapTitle}>
               <div className={styles.title}>{sample.title}</div>
               <div className={styles.company}>
